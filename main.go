80d6defb729ec3cab9c6225ad8f2e4ef9aeee84d
Kalani <cunhakalani3@gmail.com>
2024-06-19 20:53:54 -1000

    fix

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
+	"math/rand"
+	"os"
 	"time"
 )
 
 // FIX VARIABLE NAMES LATER
-// apiRequest, apiData, targetWord, jsonFormat etc
-type jsonFormat struct {
-	id                int
-	solution          string
-	days_since_launch int
-	editor            string
-}
-
+// apiRequest, apiData, targetWord, jsonFormat, number etc
 var currentDay = time.Now()
 
 func getDate() {
@@ -24,17 +17,18 @@ func getDate() {
 	fmt.Printf("Today is %s %d!", currentDay.Month(), currentDay.Day(), "\n")
 }
 
-func (targetWord string) getWordOfTheDay() {
-	data, err := http.Get(fmt.Sprintf("https://www.nytimes.com/svc/wordle/v2/%d-%d-%d.json", currentDay.Year(), currentDay.Month(), currentDay.Day()))
+func (targetWord string, downloadOption byte) getTargetWord() {
 
+	// Generate random line number
+	var number int
+	number = rand.Intn(1000)
+	fmt.Println(number) // For testing purposes, remove on commit
+
+	file, err := os.Open("src/words.txt")
 	if err != nil {
-		log.Fatalln("The API was not available at this time, maybe check your internet connection?")
+		log.Fatal(err)
 	}
 
-	err = json.Unmarshal(apiRequest, &data)
-	targetWord = data.solution
 }
-
 func main() {
-	getDate()
 }
